d5bb8a70fb4ff8a87de51421fb73ef58075f297b
Francis Aremu <Front_Francis@hotmail.com>
2017-11-30 11:45:06 +0100

    v1.2.1; Changed the return type of RETURN clause to include TResult; All Tests Passed.

--- Neo4jClient.DataAnnotations/Cypher/CypherFluentQueryExtensions`Return.cs
+++ Neo4jClient.DataAnnotations/Cypher/CypherFluentQueryExtensions`Return.cs
@@ -15,87 +15,87 @@ namespace Neo4jClient.DataAnnotations.Cypher
             return SharedProjectionQuery<TResult>(query, expression, "RETURN");
         }
 
-        public static ICypherFluentQuery Return<TResult>(this ICypherFluentQuery query, Expression<Func<TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<TResult>(this ICypherFluentQuery query, Expression<Func<TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, TResult>(this ICypherFluentQuery query, Expression<Func<T1, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, TResult>(this ICypherFluentQuery query, Expression<Func<T1, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>> expression)
+        public static ICypherFluentQuery<TResult> Return<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>> expression)
         {
             return SharedReturn<TResult>(query, expression);
         }
@@ -107,87 +107,87 @@ namespace Neo4jClient.DataAnnotations.Cypher
             return SharedProjectionQuery<TResult>(query, expression, "RETURN DISTINCT");
         }
 
-        public static ICypherFluentQuery ReturnDistinct<TResult>(this ICypherFluentQuery query, Expression<Func<TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<TResult>(this ICypherFluentQuery query, Expression<Func<TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, TResult>(this ICypherFluentQuery query, Expression<Func<T1, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, TResult>(this ICypherFluentQuery query, Expression<Func<T1, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
 
-        public static ICypherFluentQuery ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>> expression)
+        public static ICypherFluentQuery<TResult> ReturnDistinct<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this ICypherFluentQuery query, Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>> expression)
         {
             return SharedReturnDistinct<TResult>(query, expression);
         }
